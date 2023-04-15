5a94e0a75716c14cc316454480b6bab77b427c8a
snacksncode <snacksncode@gmail.com>
2023-04-15 14:53:34 +0200

    exercise C | 15.04.2023

---
+++ src/algo/z4/aprils_fun/c.py
@@ -0,0 +1,21 @@
+# Zadanie C
+program_input = [
+    (5, 'Adam'),
+    (3, 'Jane'),
+    (5, 'Xiao'),
+    (2, 'Jane')
+]
+
+
+def has_non_unique_names(program_input: list[tuple[int, str]]) -> bool:
+    names = set()
+    for entry in program_input:
+        (_id, name) = entry
+        if name in names:
+            return True
+
+        names.add(name)
+
+
+print(f"Input: {program_input}")
+print(has_non_unique_names(program_input))
