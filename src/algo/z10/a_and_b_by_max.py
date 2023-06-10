70b2e10746519b0f910e81adf6f173c7b232060e
snacksncode <snacksncode@gmail.com>
2023-06-10 09:35:28 +0200

    feat: solutions to exercises A and B writted by Max

---
+++ src/algo/z10/a_and_b_by_max.py
@@ -0,0 +1,8 @@
+# 1
+def longest_repeat(char: str) -> int:
+    return max([len(max(char.split("<"))), len(max(char.split(">")))])
+# 2
+def some_data(s: str) -> list:
+    a = [1]
+    [a.append(a[-1] + 1 if a[-1] >= 0 else - 1) if i == '<' else a.append(a[-1] - 1 if a[-1] >= 0 else +1) for i in s]
+    return a
