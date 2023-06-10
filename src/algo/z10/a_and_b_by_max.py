934f7f9da26ac470f9500696cc9c74944b6b1531
snacksncode <snacksncode@gmail.com>
2023-06-10 09:59:13 +0200

    refactor: simplify solution to problem 2

--- src/algo/z10/a_and_b_by_max.py
+++ src/algo/z10/a_and_b_by_max.py
@@ -6,10 +6,5 @@ def longest_repeat(char: str) -> int:
 # 2
 def some_data(s: str) -> list:
     a = [len(s)]
-    [
-        a.append(a[-1] + 1 if a[-1] >= 0 else -1)
-        if i == "<"
-        else a.append(a[-1] - 1 if a[-1] >= 0 else +1)
-        for i in s
-    ]
+    [a.append(a[-1] + 1) if i == "<" else a.append(a[-1] - 1) for i in s]
     return a
